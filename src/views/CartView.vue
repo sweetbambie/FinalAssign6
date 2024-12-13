b04337ddadfcf8035bba39483f05a6d96595b229
Sophia Miao <sophia@miao.fm>
2024-12-12 23:31:59 -0500

    almost forgot to put my components to cart

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,18 +1,22 @@
 <script setup>
 import { useStore } from '../store';
+import Footer from "../components/Footer.vue";
+import Header from "../components/Header.vue";
 
 const store = useStore();
 </script>
 
 <template>
-    <div class="cart">
-        <h1>Shopping Cart</h1>
-        <div class="item" v-for="([key, value]) in store.cart">
-            <img :src="`https://image.tmdb.org/t/p/w500${value.url}`" />
-            <h1>{{ value.title }}</h1>
-            <button @click="store.removeFromCart(key)">Remove</button>
-        </div>
-    </div>
+  <Header />
+  <div class="cart">
+    <h1>Shopping Cart</h1>
+      <div class="item" v-for="([key, value]) in store.cart">
+        <img :src="`https://image.tmdb.org/t/p/w500${value.url}`" />
+        <h1>{{ value.title }}</h1>
+        <button @click="store.removeFromCart(key)">Remove</button>
+      </div>
+  </div>
+  <Footer />
 </template>
 
 <style scoped>
